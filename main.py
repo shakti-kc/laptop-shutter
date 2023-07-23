16297b389d83fcab03dbf4184ef50999a67c4081
Shakti K.C <107423716+shakti-kc@users.noreply.github.com>
2023-07-23 14:58:41 +0545

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("./index.html")
+    return render_template("index.html")
 
 @app.route("/shutdown", methods=["POST"])
 def shutdown():
     os.system("shutdown /s")
     return "Magic processing..."
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
